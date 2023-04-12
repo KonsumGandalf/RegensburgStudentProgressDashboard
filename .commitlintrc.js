8c94977418e4b72e77c69784c1c757a0db32de58
David Schmidt <david2.schmidt@st.oth-regensburg.com>
2023-04-12 13:36:53 +0200

    feat(base-application): Add Husky and Commitlint
    Install Husky and Commitlint to ensure consistent commit messages in the project. Configure Commitlint to enforce the conventional commit format.
    
    Closes: #3

---
+++ .commitlintrc.js
@@ -0,0 +1,9 @@
+const {
+	utils: { getProjects },
+} = require('@commitlint/config-nx-scopes');
+
+module.exports = {
+	rules: {
+		'scope-enum': async (ctx) => [2, 'always', ['uml', 'base-application', 'devops']],
+	},
+};
