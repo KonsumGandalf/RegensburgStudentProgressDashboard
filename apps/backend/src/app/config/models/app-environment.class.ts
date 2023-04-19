89a0e9b587f82230716cf83810ca48c58ab87cc3
David Schmidt <david2.schmidt@st.oth-regensburg.com>
2023-04-19 16:14:17 +0200

    feat(base-application):
    Integrate TypeORM with Postgres for database management
    
    Refs: #16

---
+++ apps/backend/src/app/config/models/app-environment.class.ts
@@ -0,0 +1,78 @@
+import { IsEnum, IsNumber, IsString } from 'class-validator';
+
+import { HostEnum } from './host.enum';
+
+/**
+ * An interface describing the environment variables used by the application.
+ */
+export class AppEnvironment {
+    /**
+     * The port number that the application should listen on.
+     * @type {string}
+     */
+    @IsNumber()
+    APP_PORT: number;
+
+    /**
+     * The host address that the application should use.
+     * @type {HostEnum}
+     */
+    @IsEnum(HostEnum)
+    APP_HOST: HostEnum;
+
+    /**
+     * The username to use for connecting to the PostgreSQL database.
+     * @type {string}
+     */
+    @IsString()
+    POSTGRES_USER: string;
+
+    /**
+     * The password to use for connecting to the PostgreSQL database.
+     * @type {string}
+     */
+    @IsString()
+    POSTGRES_PASSWORD: string;
+
+    /**
+     * The port number to use for connecting to the PostgreSQL database.
+     * @type {string}
+     */
+    @IsNumber()
+    POSTGRES_PORT: number;
+
+    /**
+     * The host address to use for connecting to the PostgreSQL database.
+     * @type {HostEnum}
+     */
+    @IsEnum(HostEnum)
+    POSTGRES_HOST: HostEnum;
+
+    /**
+     * The name of the PostgreSQL database to connect to.
+     * @type {string}
+     */
+    @IsString()
+    POSTGRES_DB: string;
+
+    /**
+     * The username to use for connecting to the Moodle service.
+     * @type {string}
+     */
+    @IsString()
+    MOODLE_USER: string;
+
+    /**
+     * The password to use for connecting to the Moodle service.
+     * @type {string}
+     */
+    @IsString()
+    MOODLE_PASSWORD: string;
+
+    /**
+     * The base URL of the Moodle service.
+     * @type {string}
+     */
+    @IsString()
+    MOODLE_SERVICE: string;
+}
---
+++ apps/backend/src/app/config/util/config.utils.ts
@@ -0,0 +1,45 @@
+import { plainToInstance } from 'class-transformer';
+import { validateSync } from 'class-validator';
+
+import { AppConfig } from '../models/app-config.class';
+import { AppEnvironment } from '../models/app-environment.class';
+import { IAppEnvironment } from '../models/app-environment.interface';
+
+export abstract class ConfigUtils {
+    static validateEnv(rawEnvironment: IAppEnvironment): AppConfig {
+        const env = plainToInstance(AppEnvironment, rawEnvironment, {
+            enableImplicitConversion: true,
+        });
+
+        const config = ConfigUtils._createConfig(env);
+
+        const configErrors = validateSync(config, {
+            skipMissingProperties: false,
+        });
+
+        if (configErrors.length > 0) {
+            throw new Error(configErrors.toString());
+        }
+
+        return config;
+    }
+
+    private static _createConfig(environment: AppEnvironment): AppConfig {
+        return plainToInstance(AppConfig, {
+            port: environment.APP_PORT || 3333,
+            host: environment.APP_HOST || 'localhost',
+            postgresDB: {
+                host: environment.POSTGRES_HOST || 'localhost',
+                port: environment.POSTGRES_PORT || 5432,
+                name: environment.POSTGRES_DB,
+                user: environment.POSTGRES_USER,
+                password: environment.POSTGRES_PASSWORD,
+            },
+            moodle: {
+                username: environment.MOODLE_USER,
+                password: environment.MOODLE_PASSWORD,
+                service: environment.MOODLE_SERVICE,
+            },
+        } as AppConfig);
+    }
+}
