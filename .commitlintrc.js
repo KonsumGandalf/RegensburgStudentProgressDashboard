eb587d896cf9091b14db63eaabc688c5307405c9
David Schmidt <david2.schmidt@st.oth-regensburg.com>
2023-04-19 16:35:57 +0200

    feat(shared-backend-test-util): Add RepositoryMock class for simulating TypeORM repository behavior in tests
    This commit adds a RepositoryMock class to the project for simulating the behavior of a TypeORM repository in tests. The RepositoryMock class can be used as a substitute for the actual repository in tests, allowing for isolated and predictable testing of the application's database interactions.
    
    Refs: #16

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,13 +1,13 @@
 const {
-	utils: { getProjects },
+    utils: { getProjects },
 } = require('@commitlint/config-nx-scopes');
 
 module.exports = {
-	rules: {
-		'scope-enum': async (ctx) => [
-			2,
-			'always',
-			['uml', 'base-application', 'devops'],
-		],
-	},
+    rules: {
+        'scope-enum': async (ctx) => [
+            2,
+            'always',
+            ['uml', 'base-application', 'devops', 'grade-backend-github', 'shared-backend-test-util'],
+        ],
+    },
 };
