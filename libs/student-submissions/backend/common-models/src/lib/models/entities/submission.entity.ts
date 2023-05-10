4592694f74c6bbf1978ebdf837d448388a8a6ac2
David Schmidt <david2.schmidt@st.oth-regensburg.com>
2023-05-10 12:48:06 +0200

    refactor(challenge-management-backend-common-models): adjust model to new requirements of submissions

--- libs/student-submissions/backend/common-models/src/lib/models/entities/submission.entity.ts
+++ libs/student-submissions/backend/common-models/src/lib/models/entities/submission.entity.ts
@@ -1,6 +1,7 @@
+import { Assignment } from '@rspd/challenge-management/backend/common-models';
 import { BaseEntity } from '@rspd/shared/backend/utils';
 import { Student } from '@rspd/user/backend/common-models';
-import { Column, Entity, ManyToOne, TableInheritance } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne, TableInheritance } from 'typeorm';
 
 /**
  * Represents a Submission entity.
@@ -16,8 +17,6 @@ export class Submission extends BaseEntity {
 	 *
 	 * @type {number}
 	 */
-	// @Column({ type: 'int2', default: 0 })
-	// attempts: number;
 
 	@Column({ type: 'real' })
 	completionPercentage: number;
@@ -28,5 +27,18 @@ export class Submission extends BaseEntity {
 	 * @type {Student}
 	 */
 	@ManyToOne(() => Student, (student: Student) => student.submissions)
+	@JoinColumn()
 	student: Student;
+
+	/**
+	 * The number of submission a user has done
+	 *
+	 * @type {Student}
+	 */
+	@Column({ type: 'int4', default: 1 })
+	numberOfSubmissions: number;
+
+	@ManyToOne(() => Assignment, (assignment: Assignment) => assignment.submissions)
+	@JoinColumn()
+	assignment: Assignment;
 }
